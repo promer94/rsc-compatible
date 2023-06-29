f920007f9e5b69930106ff144751a78a05f4df34
Yixuan Xu <yixuanxu94@outlook.com>
2023-06-29 21:40:56 +0800

    update

--- apps/web/app/page.tsx
+++ apps/web/app/page.tsx
@@ -6,7 +6,7 @@ import { shared } from 'hybrid-lib'
 export default function Page() {
   return (
     <div className='p-4 border-4 border-red-200'>
-      <div>server components</div>
+      <div>server component</div>
       <div>{serverOnly()}</div>
       <div>{shared()}</div>
       <div>{`import { clientOnly } from "client-lib" ❌`}</div>
